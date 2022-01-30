db83966de63ef9f6001de013fb9f0b4937e577d6
siddhantprateek <siddhantprateek@gmail.com>
2022-01-30 17:02:48 +0530

    rating

--- client/src/components/teacher-profile/teacherProfile.components.jsx
+++ client/src/components/teacher-profile/teacherProfile.components.jsx
@@ -1,19 +1,18 @@
-import './teacherProfile.styles.css';
-const TeacherProfile = ({Name, source}) => {
-    return(
-        <div className="teacher-profile">
-            <div className="teacher-container">
-                <img src={source} alt="" />
-                <h2>{Name}</h2>
-            </div>
-            <div className="input-div">
-            <textarea 
-                type="text" 
-                placeholder='Enter your review'
-                />
-            </div>
-        </div>
-    )
-}
+import "./teacherProfile.styles.css";
+import Rating from "../rating/rating.components";
+const TeacherProfile = ({ Name, source }) => {
+  return (
+    <div className="teacher-profile">
+      <div className="teacher-container">
+        <img src={source} alt="" />
+        <h2>{Name}</h2>
+      </div>
+      <Rating/> 
+      <div className="input-div">
+        <textarea type="text" placeholder="Enter your review" />
+      </div>
+    </div>
+  );
+};
 
 export default TeacherProfile;

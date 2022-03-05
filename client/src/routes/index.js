44900a2ac3028496507a85909d5853837ff25acf
siddhantprateek <siddhantprateek@gmail.com>
2022-03-05 23:07:35 +0530

    🚩export components

---
+++ client/src/routes/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './home/home.routes';
+export { default as Dashboard } from './dashboard/dashboard.routes';
+export { default as Events } from './Events/Events.routes';
+export { default as Profile } from './profile/profile.routes';
+export { default as Nomatch } from './nomatch/nomatch.routes';

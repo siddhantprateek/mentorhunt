55467893d0c194e06521166a269e7eec88398297
siddhantprateek <siddhantprateek@gmail.com>
2022-03-14 17:11:14 +0530

    🚩Removed the Overlapping of text

--- client/src/components/EventBadge/EventBadge.components.jsx
+++ client/src/components/EventBadge/EventBadge.components.jsx
@@ -3,14 +3,16 @@ import { Button } from '../../components';
 const EventBadge = () => {
   return (
     <div className='event-container'>
-        <div className="event-thumbnail"></div>
+        <div className="event-thumbnail">
+          
+        </div>
         <div className="event-contents">
             <h1>Event 1</h1>
             <hr />
             <div className='event-props'>
-                <p>Sat, June 12, 2022, 7:30 PM (IST)</p>
+                <p>Sat, June 12, 2022, 7:30 PM (IST)</p>             
                 <p>Venue: Online</p>
-            <p>Hosted by:</p>
+                <p>Hosted by:</p>
             </div>
             <div className="event-schedule">
                 <Button pholder="Join Event"/>

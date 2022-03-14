f618047c94c7fd629ee2e1792b812d1ab1236da3
siddhantprateek <siddhantprateek@gmail.com>
2022-03-14 17:11:33 +0530

    🚩Added Contributors to Project

--- client/src/components/footer/footer.components.jsx
+++ client/src/components/footer/footer.components.jsx
@@ -6,12 +6,20 @@ const Footer = () => {
     return (
     <div className="footer">
         <div className="footer-container">
-            <a href="https://github.com/siddhantprateek/rteach">
-                <img src={`https://img.icons8.com/ios-glyphs/30/000000/github.png`} alt=""/>
-            </a>
-            <p>Creator - Siddhant Prateek Mahayanayak</p>
-            <p>&#169; All Copyrights Reserved, {year} </p>
-
+            <div className='left-footer'>
+                <a href="https://github.com/siddhantprateek/rteach">
+                    <img src={`https://img.icons8.com/ios-glyphs/30/000000/github.png`} alt=""/>
+                </a>
+                
+                <p>Maintainer - Siddhant Prateek Mahayanayak</p>
+                <p>&#169; All Copyrights Reserved, {year} </p>
+            </div>
+            <div className="right-footer">
+                <p><b>Contributors</b></p>
+                <a href="https://github.com/siddhantprateek/rteach/graphs/contributors">
+                    <img src="https://contrib.rocks/image?repo=siddhantprateek/rteach" height="30px" alt=""/>
+                </a>
+            </div>
         </div>
     </div>
   )
